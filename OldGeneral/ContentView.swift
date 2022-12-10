f676fd75c6ddb514d1f4bee86dba2444a13d3faf
liyuqi <a964581583@hotmail.com>
2022-12-10 13:30:36 +0800

    init

---
+++ OldGeneral/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  OldGeneral
+//
+//  Created by 李毓琪 on 2022/12/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, swift!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OldGeneral/OldGeneralApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldGeneralApp.swift
+//  OldGeneral
+//
+//  Created by 李毓琪 on 2022/12/10.
+//
+
+import SwiftUI
+
+@main
+struct OldGeneralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
